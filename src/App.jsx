ad5014a1fee5a08540b114714a615de4ed2b7272
Laremo <memoo.lara12306@hotmail.com>
2023-11-08 12:01:37 -0600

    Location Form and Characters display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 import MainContainer from './Components/Main-Container/main-container';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <MainContainer />
